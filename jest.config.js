0bbef3664455b92b1be272d59e5ebf7013e9a644
shichang.qiu <shichang.qiu@idetail.cn>
2021-11-03 01:27:17 +0800

    feat(deps                           (项目依赖)): jest 配置

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,11 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
 	transform: {
+		'^.+\\.js$': 'babel-jest',
 		'^.+\\.vue$': 'vue-jest', // vue 文件用 vue-jest 转换
 		'^.+\\.ts$': 'ts-jest' // ts 文件用 ts-jest 转换
 	},
 	// 匹配 __tests__ 目录下的 .js/.ts 文件 或其他目录下的 xx.test.js/ts xx.spec.js/ts
-	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)|(js)$'
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js)$',
+	transformIgnorePatterns: ['<rootDir>/bower_components/', '<rootDir>/node_modules/']
 }
